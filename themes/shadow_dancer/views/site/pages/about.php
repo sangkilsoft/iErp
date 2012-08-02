ae3f2bf7aeb5f6f219ea1fd5037fa525191350da
mujib <mujib@SangkilPC>
2012-08-02 15:55:54 +0700

    Create new theme

---
+++ themes/shadow_dancer/views/site/pages/about.php
@@ -0,0 +1,10 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Calendar';
+$this->breadcrumbs=array(
+	'Calendar',
+);
+?>
+<h1>Calendar</h1>
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <tt><?php echo __FILE__; ?></tt>.</p>
