ea4f9bdf77deb8c5a4c2a933f22c0176adf28868
mujib <mujib@SangkilPC>
2012-08-03 17:31:35 +0700

    update mbmenu

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -34,7 +34,7 @@ class UserIdentity extends CUserIdentity {
         /*
          * Set menu according to user role
          * make generator function from database model
-         * set static for now
+         * set static for now again
          */
         $this->_menu = array(
             'items' => array(
