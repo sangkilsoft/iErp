59f9cab8f645fba270069b985a2b94054299c0bd
mujib <mujib@SangkilPC>
2012-08-04 11:52:01 +0700

    standarize button theme

--- themes/shadow_dancer/views/whse/_form.php
+++ themes/shadow_dancer/views/whse/_form.php
@@ -1,39 +1,41 @@
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'whse-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'whse-form',
+        'enableAjaxValidation' => false,
+            ));
+    ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+    <?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'cd_whse'); ?>
-		<?php echo $form->textField($model,'cd_whse',array('size'=>4,'maxlength'=>4)); ?>
-		<?php echo $form->error($model,'cd_whse'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'cd_whse'); ?>
+        <?php echo $form->textField($model, 'cd_whse', array('size' => 4, 'maxlength' => 4)); ?>
+        <?php echo $form->error($model, 'cd_whse'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'nm_whse'); ?>
-		<?php echo $form->textField($model,'nm_whse',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'nm_whse'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'nm_whse'); ?>
+        <?php echo $form->textField($model, 'nm_whse', array('size' => 32, 'maxlength' => 32)); ?>
+        <?php echo $form->error($model, 'nm_whse'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'create_by'); ?>
-		<?php echo $form->textField($model,'create_by'); ?>
-		<?php echo $form->error($model,'create_by'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'create_by'); ?>
+        <?php echo $form->textField($model, 'create_by'); ?>
+        <?php echo $form->error($model, 'create_by'); ?>
+    </div>
 
-	<div class="row buttons">
-		<?php
-                //echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); 
-                echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class'=>'btn-orange'));
-                ?>
-	</div>
+    <div class="row buttons">
+        <?php
+        //echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); 
+        echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn-orange'));
+        ?>
+    </div>
 
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div><!-- form -->
