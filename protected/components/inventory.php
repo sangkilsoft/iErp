3e813a0e7884b4f754caca33d778ce832878a159
sangkilsoft <masyhudi.melik@gmail.com>
2012-10-05 18:18:22 +0700

    implement master-detail form

--- protected/components/inventory.php
+++ protected/components/inventory.php
@@ -8,10 +8,7 @@
 class inventory extends CComponent {
 
     //put your code here
-    public function setReceipt($hdr = array(), $dtl = array()) {
-//        if(!count($dtl)>0)
-//            return array('type' => 'E', 'message' => 'Detail GR must not be blank..');
-
+    public function createReceipt($hdr = array(), $dtl = array()) {
         $mdl = new GoodReceipt;
         $mdl->attributes = $hdr;
         if ($mdl->save()) {
@@ -23,7 +20,30 @@ class inventory extends CComponent {
                 $mdldtl->qty_trans = $dtl['qty_trans'][$i];
                 $mdldtl->id_uoms = $dtl['id_uoms'][$i];
                 $mdldtl->id_locator = $dtl['id_locator'][$i];
-                $mdldtl->item_line = $dtl['item_line'][$i];
+                $mdldtl->value_trans = $dtl['value_trans'][$i];
+                if (!$mdldtl->save())
+                    return array('type' => 'E', 'message' => 'Error on Insert', 'val' => $mdldtl->getErrors());
+                $i++;
+            }
+            return array('type' => 'S', 'message' => 'Successfully create receipt, doc.number:' . $mdl->gr_num, 'val' => $mdl);
+        }else
+            return array('type' => 'E', 'message' => 'Error on Insert', 'val' => $mdl->getErrors());
+    }
+
+    public function updateReceipt($hdr = array(), $dtl = array()) {
+        $mdl = new GoodReceipt;
+        $mdl = $mdl->model()->findAllByPk($hdr['id_receipt']);
+        $mdl->attributes = $hdr;
+        if ($mdl->save()) {
+            $i = 0;
+            foreach ($dtl['id_product'] as $value) {
+                $mdldtl = new GrLine;
+                $mdldtl = $mdldtl->model()->findAllByPk($hdr['id_line']);
+                $mdldtl->id_receipt = $mdl->id_receipt;
+                $mdldtl->id_product = $dtl['id_product'][$i];
+                $mdldtl->qty_trans = $dtl['qty_trans'][$i];
+                $mdldtl->id_uoms = $dtl['id_uoms'][$i];
+                $mdldtl->id_locator = $dtl['id_locator'][$i];
                 $mdldtl->value_trans = $dtl['value_trans'][$i];
                 if (!$mdldtl->save())
                     return array('type' => 'E', 'message' => 'Error on Insert', 'val' => $mdldtl->getErrors());
