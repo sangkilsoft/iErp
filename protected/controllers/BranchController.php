485e885707a30f20f3ed38ca437f13a7ada0e497
sangkilsoft <masyhudi.melik@gmail.com>
2012-10-29 16:11:32 +0700

    branch control commit

--- protected/controllers/BranchController.php
+++ protected/controllers/BranchController.php
@@ -29,7 +29,7 @@ class BranchController extends Controller {
                 'users' => array('*'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('create', 'update', 'optionBranch', 'poDBranch'),
+                'actions' => array('create', 'update', 'optionBranch','poDBranch'),
                 'users' => array('@'),
             ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
