cb26db98ebca0ec9c6ac035d2a50636aae8cb17c
sangkilsoft <masyhudi.melik@gmail.com>
2012-11-12 19:26:50 +0700

    sales

--- protected/controllers/SalesController.php
+++ protected/controllers/SalesController.php
@@ -8,80 +8,169 @@ class SalesController extends Controller {
         $this->render('index');
     }
 
-    // Uncomment the following methods and override them if needed
-    /*
-      public function filters()
-      {
-      // return the filter configuration for this controller, e.g.:
-      return array(
-      'inlineFilterName',
-      array(
-      'class'=>'path.to.FilterClass',
-      'propertyName'=>'propertyValue',
-      ),
-      );
-      }
-
-      public function actions()
-      {
-      // return external action classes, e.g.:
-      return array(
-      'action1'=>'path.to.ActionClass',
-      'action2'=>array(
-      'class'=>'path.to.AnotherActionClass',
-      'propertyName'=>'propertyValue',
-      ),
-      );
-      }
+    /**
+     * @return array action filters
      */
-    public function actionSalRequest() {
-        $model = new SalRequest;
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
 
-        // uncomment the following code to enable ajax-based validation
-        /*
-          if(isset($_POST['ajax']) && $_POST['ajax']==='sal-request-salRequest-form')
-          {
-          echo CActiveForm::validate($model);
-          Yii::app()->end();
-          }
-         */
-
-        if (isset($_POST['SalRequest'])) {
-            $model->attributes = $_POST['SalRequest'];
-            if ($model->validate()) {
-                // form inputs are valid, do something here
-                return;
-            }
-        }
-        $this->render('salRequest', array('model' => $model));
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('index'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'users' => array('@'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
     }
 
     public function actionSalesOrder() {
+        $this->layout = '//layouts/column1';
         $model = new SalOrder;
+        $model->status = 0;
         $modeldtl = new SorderLine;
-        // uncomment the following code to enable ajax-based validation
-        /*
-          if(isset($_POST['ajax']) && $_POST['ajax']==='sal-order-salesOrder-form')
-          {
-          echo CActiveForm::validate($model);
-          Yii::app()->end();
-          }
-         */
+        $modelinfo = new SorderInfo;
 
-        if (isset($_POST['SalOrder'])) {
-            $model->attributes = $_POST['SalOrder'];
-            if ($model->validate()) {
-                // form inputs are valid, do something here
-                return;
+        if (!isset($_POST['batal'])) {
+            if (isset($_POST['SalOrder'])) {
+                $model->attributes = $_POST['SalOrder'];
+                $model->order_date = $this->dateConvert($_POST['SalOrder']['order_date']);
+                $model->delivery_schdate = $this->dateConvert($_POST['SalOrder']['delivery_schdate']);
+
+                $model->id_orgn = Yii::app()->user->orgn;
+                $model->id_branch = Yii::app()->user->branch;
+
+                //$modeldtl = $_POST['items'];
+                //$modelinfo->attributes = $_POST['SorderInfo'];
+                if (isset($_POST['search_x'])) {
+                    $ada = SalOrder::model()->exists('so_num=:so_num', array(':so_num' => $model->so_num));
+                    if ($ada)
+                        $model = SalOrder::model()->find('so_num=:so_num', array(':so_num' => $model->so_num));
+                }
+
+                $conn = Yii::app()->db->beginTransaction();
+                try {
+                    if (isset($_POST['btnsave'])) {
+                        $mycois = $_POST['btnsave'];
+                        switch ($mycois) {
+                            case "Save":
+                                //creating
+                                if ($model->save())
+                                    Yii::app()->user->setFlash('success', 'doc.number:' . $model->so_num . ' created..');
+                                break;
+                            case "Approve":
+                                $model = SalOrder::model()->find('so_num=:so_num', array(':so_num' => $model->so_num));
+                                $model->attributes = $_POST['SalOrder'];
+                                $model->order_date = $this->dateConvert($_POST['SalOrder']['order_date']);
+                                $model->delivery_schdate = $this->dateConvert($_POST['SalOrder']['delivery_schdate']);
+
+                                $model->id_orgn = Yii::app()->user->orgn;
+                                $model->id_branch = Yii::app()->user->branch;
+
+                                $model->status = 1;
+                                //update to Approved
+                                if ($model->save())
+                                    Yii::app()->user->setFlash('success', 'DO Approved, doc.number:' . $model->so_num);
+                                break;
+                            case "Release":
+                                /*
+                                 * Check credit limit, outstanding invoiced, dll.
+                                 */
+                                $model = SalOrder::model()->find('so_num=:so_num', array(':so_num' => $model->so_num));
+                                $model->attributes = $_POST['SalOrder'];
+                                $model->order_date = $this->dateConvert($_POST['SalOrder']['order_date']);
+                                $model->delivery_schdate = $this->dateConvert($_POST['SalOrder']['delivery_schdate']);
+
+                                $model->id_orgn = Yii::app()->user->orgn;
+                                $model->id_branch = Yii::app()->user->branch;
+
+                                $model->status = 2;
+                                //Update to Released
+                                if ($model->save())
+                                    Yii::app()->user->setFlash('success', 'DO Released, doc.number:' . $model->so_num);
+                                break;
+                            case "Post (Create Invoice)":
+                                /*
+                                 * Check shipping first
+                                 */
+                                $model = SalOrder::model()->find('so_num=:so_num', array(':so_num' => $model->so_num));
+                                $model->attributes = $_POST['SalOrder'];
+                                $model->order_date = $this->dateConvert($_POST['SalOrder']['order_date']);
+                                $model->delivery_schdate = $this->dateConvert($_POST['SalOrder']['delivery_schdate']);
+
+                                $model->id_orgn = Yii::app()->user->orgn;
+                                $model->id_branch = Yii::app()->user->branch;
+
+                                if ($model->status < 3)
+                                    Yii::app()->user->setFlash('error', 'doc.number:' . $model->so_num . ' belum terkirim..');
+                                else {
+                                    $model->status = 4;
+                                    //Update to posting
+                                    if ($model->save())
+                                        Yii::app()->user->setFlash('success', 'DO Released, doc.number:' . $model->so_num);
+                                }
+                                break;
+                            default :
+                                Yii::app()->user->setFlash('error', 'doc.number:' . $model->so_num . ' failed while update..');
+                        }
+                    }
+                    $conn->commit();
+                } catch (Exception $exc) {
+                    echo $exc->getTraceAsString();
+                }
+                if (isset($model->order_date))
+                    $model->order_date = $this->dateRevert($model->order_date);
+                if (isset($model->delivery_schdate))
+                    $model->delivery_schdate = $this->dateRevert($model->delivery_schdate);
             }
         }
-        $this->render('salesOrder', array('model' => $model, 'modeldtl' => $modeldtl));
+
+        $sql = "SELECT value FROM configure WHERE conf_name = 'so_status' AND trim(conf_code)='$model->status'";
+        $command = Yii::app()->db->createCommand($sql);
+        $val = $command->queryScalar();
+
+        if (!$model->isNewRecord) {
+            $mcust = Customers::model()->find('id_customer=:id_cust', array(':id_cust' => $model->id_customer));
+            $cust['cd_cust'] = $mcust->cd_cust;
+            $cust['nm_cust'] = $mcust->nm_cust;
+        } else {
+            $cust['cd_cust'] = '';
+            $cust['nm_cust'] = '';
+        }
+        $this->render('salesOrder', array('model' => $model,
+            'modeldtl' => $modeldtl,
+            'modelinfo' => $modelinfo,
+            'status_val' => $val,
+            'cust' => $cust));
     }
 
     public function actionCustomerList() {
-        $tampil = CHtmlPurifier::
-                $this->renderPartial('customerList', array(), false, false);
+        $criteria = new CDbCriteria;
+        $cust = new CActiveDataProvider('Customers', array(
+                    'criteria' => $criteria,
+                ));
+
+        //$this->render('customerList',  array('cust'=>$cust), false, false);
+        $tampil = $this->renderPartial('customerList', array('cust' => $cust), false, false);
         echo $tampil;
+    }
+
+    protected function setItem($mld, $val) {
+        return $mdl;
     }
 
 }
